6db89b9c80b315ae30ce62e3efe4d45a1fcecc18
Nagim123 <fourcolorsgame@gmail.com>
2023-06-20 01:13:13 +0300

    Fix spaces and add multi right answer!

--- quiz_craft_package/models/MCQ_generator.py
+++ quiz_craft_package/models/MCQ_generator.py
@@ -9,32 +9,17 @@ class MCQGenerator:
         self.instruction = """
             You are multiple choice question generator. You will get text from user and you must create multiple choice questions based on text provided by user.
             Questions should be based on what the text is about, not how the author writes it. Questions should test knowledge of what the text is trying to teach the reader.
-            Strictly respond in format and do not add extra line breaks (If you can make more than one question start each question in a new line):
-            Question text
-            A: Answer A
-            B: Answer B
-            C: Answer C
-            D: Answer D (You can add more options if required just add other english alphabet letters)
-            Answer: AB (enumerate all right answer letters without spaces or write just 1 letter if only 1 answer is true)
-            (Do not add line break between answer options and true answer enumeration.)
+            Respond in python array like style where first element is question itself then subarray contains options to answer and last element is subarray that has indexes of right answers of options in first subarray.
+            Example:
+            ["Question text", ["option A", "option B", "option C", "option D"], [0, 2]]
         """
         self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     def generate_question(self, text: str):
         output = self.llm([SystemMessage(content=self.instruction), HumanMessage(content=text)])
-        print(output.content)
-        output = output.content.split("\n\n")
+        #print(output.content)
+        output = output.content.split("\n")
         questions = []
         for question_raw in output:
-            question_splitted = question_raw.split("\n")
-            question = question_splitted[0]
-            options = []
-            for i in range(1, len(question_splitted)-1):
-                options.append(question_splitted[i][3:])
-            letters = question_splitted[-1].split(": ")[1]
-            true_answers = []
-            for letter in letters:
-                true_answers.append(self.alphabet.find(letter))
-            questions.append([question, options, true_answers])
-            
+            questions.append(eval(question_raw))
         return questions
