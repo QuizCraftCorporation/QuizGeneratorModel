b55e23ac64ecf4b397bfb2a878bddad399c2a73c
Nagim123 <fourcolorsgame@gmail.com>
2023-06-20 19:23:53 +0300

    Add syntax question checking

--- quiz_craft_package/models/MCQ_generator.py
+++ quiz_craft_package/models/MCQ_generator.py
@@ -17,12 +17,25 @@ class MCQGenerator:
 
     def generate_question(self, text: str):
         output = self.llm([SystemMessage(content=self.instruction), HumanMessage(content=text)])
-        print(output.content)
+        #print(output.content)
         output = output.content.replace("\n\n", "\n").split("\n")
         questions = []
         for question_raw in output:
             try:
-                questions.append(eval(question_raw))
+                generated_question = eval(question_raw)
+                if self._check_if_question_is_valid(generated_question):
+                    questions.append(generated_question)
+                else:
+                    print(generated_question)
             except:
                 continue
         return questions
+
+    def _check_if_question_is_valid(self, question: list):
+        if type(question) is list:
+            if len(question) == 3:
+                if type(question[0]) is str and type(question[1]) is list and type(question[2]) is list:
+                    if len(question[1]) >= len(question[2]):
+                        if max(question[2]) < len(question[1]) and min(question[2]) > -1:
+                            return True
+        return False
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from pprint import pprint
 from quiz_craft_package.quiz_generator import QuizGenerator
 
-MATERIAL_TEXT_FILE_PATH = "./data/material.txt"
+MATERIAL_TEXT_FILE_PATH = "./data/example.pdf"
 MATERIAL_TEXT_FILE_PATH_2 = "./data/material_2.txt"
 RESULT_FILE_PATH = "./output/result.txt"
 
 q_gen = QuizGenerator(debug=True)
-result = q_gen.create_questions_from_files([MATERIAL_TEXT_FILE_PATH, MATERIAL_TEXT_FILE_PATH_2])
+result = q_gen.create_questions_from_files([MATERIAL_TEXT_FILE_PATH])
 
-pprint(result)
+#pprint(result)
 
 output_str = ""
 for q in result:
