d406b74306bc0516d6da90ec9d53d597aa0f7024
Nagim123 <fourcolorsgame@gmail.com>
2023-06-30 00:25:21 +0300

    Add text results

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from quiz_craft_package.quiz_generator import QuizGenerator
 
-MATERIAL_TEXT_FILE_PATH = "./data/material.txt"
+MATERIAL_TEXT_FILE_PATH = "./data/example.pdf"
 MATERIAL_TEXT_FILE_PATH_2 = "./data/material_2.txt"
-RESULT_FILE_PATH = "./output/result.txt"
+RESULT_FILE_PATH = "./output/music.txt"
 
 q_gen = QuizGenerator(debug=True)
-result = q_gen.create_quiz_from_files([MATERIAL_TEXT_FILE_PATH, MATERIAL_TEXT_FILE_PATH_2], max_questions=1)
+result = q_gen.create_quiz_from_files([MATERIAL_TEXT_FILE_PATH], max_questions=8)
+
+for question in result:
+    print(str(question))
 
 result_file = open(RESULT_FILE_PATH, "w", encoding="utf8")
 result_file.write(str(result))
