0d0e83513b3b8578aafdd4055fd6fb644e886a8a
Nagim123 <fourcolorsgame@gmail.com>
2023-06-30 13:10:06 +0300

    Add unique ids to quiz_database

--- quiz_craft_package/quiz_database.py
+++ quiz_craft_package/quiz_database.py
@@ -1,6 +1,7 @@
 import os
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.schema import Document
 from .containers.nagim_quiz import NagimQuiz
 
 class QuizDataBase:
@@ -15,26 +16,38 @@ class QuizDataBase:
         if os.path.exists(os.path.join(save_folder_path, "index.faiss")):
             self.db = FAISS.load_local(save_folder_path, self.embeddings)
 
-    def save_quiz(self, quiz: NagimQuiz) -> None:
+    def save_quiz(self, quiz: NagimQuiz, unique_id: str) -> None:
         """
         Save a quiz into vector database.
+
+        Args:
+            quiz (NagimQuiz): Quiz to save.
+            unique_id (str): Some unique string to identify your quiz while search.
         """
         quiz_full_text = str(quiz)
+        new_doc = Document(page_content=quiz_full_text, metadata=dict(unique_id=unique_id))
 
         if self.db == None:
-            self.db = FAISS.from_texts([quiz_full_text], self.embeddings)
-        else:
-            self.db.add_texts([quiz_full_text])
+            self.db = FAISS.from_documents([new_doc], self.embeddings)
+        else:    
+            self.db.add_documents([new_doc])
         
         self.db.save_local(self.save_folder_path)
 
     def search_quiz(self, query: str, number_of_results=4) -> list[NagimQuiz]:
         """
         Perform a search inside a vector database to retrieve quiz.
+
+        Args:
+            query (str): Search query.
+            number_of_results (int): Maximum number of quizzes to return.
+
+        Returns:
+            list[NagimQuiz]: A list with quizzes that are the most appropriate to search query.
         """
         docs = self.db.similarity_search(query, k=number_of_results)
         the_most_similar_quizzes = []
         for doc in docs:
-            the_most_similar_quizzes.append(NagimQuiz.from_string(doc.page_content))
+            the_most_similar_quizzes.append((NagimQuiz.from_string(doc.page_content), doc.metadata["unique_id"]))
         return the_most_similar_quizzes
 
