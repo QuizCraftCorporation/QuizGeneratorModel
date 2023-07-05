641969fa597fe0d99904a8244d54ea93e07ad67f
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-07-05 19:11:43 +0300

    Change testing

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from quiz_craft_package.quiz_generator import QuizGenerator
 
-MATERIAL_TEXT_FILE_PATH = "./data/example.pdf"
+MATERIAL_TEXT_FILE_PATH = "./data/material.txt"
 MATERIAL_TEXT_FILE_PATH_2 = "./data/material_2.txt"
-RESULT_FILE_PATH = "./output/music.txt"
+RESULT_FILE_PATH = "./output/result.txt"
 
 q_gen = QuizGenerator(debug=True)
-result = q_gen.create_quiz_from_files([MATERIAL_TEXT_FILE_PATH], max_questions=8)
+result = q_gen.create_quiz_from_files([MATERIAL_TEXT_FILE_PATH])
 
 for question in result:
     print(str(question))
