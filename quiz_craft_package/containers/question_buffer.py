2abe8ae3a118386f3c83e361f94be25b7bb87a3c
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-07-05 19:04:46 +0300

    Document question buffer

--- quiz_craft_package/containers/question_buffer.py
+++ quiz_craft_package/containers/question_buffer.py
@@ -12,6 +12,9 @@ class QuestionBuffer:
     def _flatten(self) -> list[NagimQuestion]:
         """
         Converet 2D question buffer to 1D with follow of importance order.
+            
+        Returns:
+            list[NagimQuestion]: 1-d list of questions sorted by importance.
         """
         if len(self._question_chunks) == 0:
             return []
@@ -27,11 +30,27 @@ class QuestionBuffer:
         return buffer
 
     def get_best_questions(self, k: int) -> list[NagimQuestion]:
+        """
+        Get best k questions.
+
+        Args:
+            k (int): Maximum number of best questions to return.
+            
+        Returns:
+            list[NagimQuestion]: Top k best questions list.
+        """
         return self._flatten()[:k]
 
     def divide_chunk(self, question_chunk: list[NagimQuestion], extract_count: int) -> list[NagimQuestion]:
         """
-        Divide chunk into buffer part and extract part.
+        Divide chunk into buffer part and extract part. Use it if you want to buffer part of questions for future utilization.
+
+        Args:
+            question_chunk (list[NagimQuestion]): Chunk of questions.
+            extract_count (int): How much questions must be in extract part.
+            
+        Returns:
+            list[NagimQuestion]: Extract part of questions.
         """
         
         if len(question_chunk) < extract_count:
