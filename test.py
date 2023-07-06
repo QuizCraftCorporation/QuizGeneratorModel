88b53076894059eaa8187bb9b28194dadcdc8705
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-07-06 20:05:26 +0300

    Add fairy tail text

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from quiz_craft_package.quiz_generator import QuizGenerator
 
-MATERIAL_TEXT_FILE_PATH = "./data/material.txt"
+MATERIAL_TEXT_FILE_PATH = "./data/fairy_tale.txt"
 MATERIAL_TEXT_FILE_PATH_2 = "./data/material_2.txt"
-RESULT_FILE_PATH = "./output/result.txt"
+RESULT_FILE_PATH = "./output/fairy_tale.txt"
 
 q_gen = QuizGenerator(debug=True)
 result = q_gen.create_quiz_from_files([MATERIAL_TEXT_FILE_PATH])
